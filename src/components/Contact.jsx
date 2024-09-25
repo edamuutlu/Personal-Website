02b3a625536eeaf86ff387ac062f208f0626d876
Eda Nur Mutlu <112180102+edamuutlu@users.noreply.github.com>
2024-09-25 18:46:16 +0300

    Dependency changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,10 +26,6 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    console.log('Service ID:', import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
-  console.log('Template ID:', import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-  console.log('Public Key:', import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
-
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
